10e8af29cb89c9bd75adf72620a7d964d2caee7c
Callum McLuskey <callummcluskey100@gmail.com>
2023-11-27 23:42:04 +0000

    ✨ added `terrible` comments that are unhelpful

--- enumerable.go
+++ enumerable.go
@@ -1,6 +1,5 @@
 package js_array_methods
 
-
 type Enumerable[T any] struct {
 	items []T
 }
@@ -9,12 +8,14 @@ func NewEnumerable[T any](items []T) Enumerable[T] {
 	return Enumerable[T]{items: items}
 }
 
+// Call an anonymous function across each element in your list of items to change values.
 func (e *Enumerable[T]) Map(fn func(*T)) {
 	for i := range e.items {
 		fn(&(e.items[i]))
 	}
 }
 
+// Call an anonymous function across each element in your list of items to filter out unwanted items.
 func (e *Enumerable[T]) Filter(fn func(*T) bool) {
 	copy_items := make([]T, 0)
 
@@ -26,6 +27,9 @@ func (e *Enumerable[T]) Filter(fn func(*T) bool) {
 	e.items = copy_items
 }
 
+// Squash your list of items using a criteria specified in a callback method using a starting value.
+//
+// - Outputs a value unlike the rest of the functional methods in this package
 func (e *Enumerable[T]) Reduce(fn func(*T, *T) T, starting_point T) T {
 	prev := starting_point
 	curr := starting_point
@@ -37,4 +41,3 @@ func (e *Enumerable[T]) Reduce(fn func(*T, *T) T, starting_point T) T {
 
 	return prev
 }
-
